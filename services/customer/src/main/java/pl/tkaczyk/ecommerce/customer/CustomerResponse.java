0cb723c5bea8645c9df6768c026fabf71cea44bb
Patryk Tkaczyk <patryk.tkaczyk@ug.edu.pl>
2024-10-22 14:03:57 +0200

    init commit

---
+++ services/customer/src/main/java/pl/tkaczyk/ecommerce/customer/CustomerResponse.java
@@ -0,0 +1,10 @@
+package pl.tkaczyk.ecommerce.customer;
+
+public record CustomerResponse(
+        String id,
+        String firstname,
+        String lastname,
+        String email,
+        Address address
+) {
+}
---
+++ services/customer/src/test/java/pl/tkaczyk/ecommerce/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tkaczyk.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
